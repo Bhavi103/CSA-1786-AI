b108bc2b89d337cf8d80838896d0d8ecb7d66b0b
Bhavi103 <113913725+Bhavi103@users.noreply.github.com>
2023-02-07 00:32:58 +0530

    Update fruits.pl

--- fruits.pl
+++ fruits.pl
@@ -7,8 +7,3 @@ colour(X, unknown).
 
 predict_fruit(X, C) :- colour(X, C), !.
 predict_fruit(X, unknown).
-location(desk, office).
-location(apple, kitchen).
-location(flashlight, desk).
-
-find_location(Item, Location) :- location(Item, Location).
